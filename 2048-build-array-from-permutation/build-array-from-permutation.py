18a7bf09cfc706227b3258ae72d6389f084729c6
athulal047-byte <athulal047@gmail.com>
2025-07-26 00:20:16 +0530

    Time: 4 ms (42.70%) | Memory: 12.6 MB (62.97%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def buildArray(self, nums):
         ans=[]
-        rough=[]
         for i in range(len(nums)):
             ans.append(nums[nums[i]])
         return ans
