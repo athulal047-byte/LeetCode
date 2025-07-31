336f29f21c79be5be22ccf69b3b30d3d7b1d2939
athulal047-byte <athulal047@gmail.com>
2025-08-01 01:31:48 +0530

    Time: 36 ms (78.08%) | Memory: 12.5 MB (70.43%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def shuffle(self, nums, n):
         list=[]
-        f=2*n
-        g=n+1
         for i in range(n):
             list.append(nums[i])
             list.append(nums[i+n])
