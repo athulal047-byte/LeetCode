b76f30487118886d549b556c063c00146c40b592
athulal047-byte <athulal047@gmail.com>
2026-07-18 20:49:57 +0530

    Time: 8 ms (40.54%) | Memory: 13.5 MB (76.56%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        l=0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                continue
+            else:
+                nums[l],nums[r]=nums[r],nums[l]
+                l=l+1
+        return nums
+        
